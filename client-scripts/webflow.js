2ba0a67541d57cd5927e104e0846307fee933913
GitHub Action <action@github.com>
2023-07-04 07:19:33 +0000

    Update from private repo

--- client-scripts/webflow.js
+++ client-scripts/webflow.js
@@ -72,9 +72,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 const response = yield fetch(`${getApiUrl()}/api/comments/add`, {
                     method: 'POST',
                     headers: {
-                        'Content-Type': 'application/json',
-                        Accept: 'application/json',
-                        'Access-Control-Allow-Origin': '*'
+                        'Content-Type': 'application/json'
                     },
                     body: JSON.stringify(commentData)
                 });

f3a19c608a5802f93e4d4a17d7873ee6dadad88d
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2023-07-03 23:22:37 -0700

    edit

--- build/client-scripts/constants.js
+++
@@ -1,4 +0,0 @@
-export const DEV_MODE_VARIABLE_NAME = 'DEV_MODE';
-export const LOCAL_API_URL = 'http://localhost:5173';
-export const PROD_API_URL = 'https://comment-bae-3fa3e.web.app';
-//# sourceMappingURL=constants.js.map

d6af61b1203dc5b2e67315f512852d94258f040a
GitHub Action <action@github.com>
2023-07-06 19:07:19 +0000

    Update from private repo

--- client-scripts/webflow.js
+++ client-scripts/webflow.js
@@ -9,41 +9,45 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 import { formatDistanceToNow, getApiUrl } from './helpers.js';
 import { FORM_PREFIX, FORM_SELECTORS, TEMPLATE_PREFIX, TEMPLATE_SELECTORS } from './selectors.js';
+const fetchComments = () => __awaiter(void 0, void 0, void 0, function* () {
+    const hostname = window.location.hostname;
+    const pathname = window.location.pathname;
+    const response = yield fetch(`${getApiUrl()}/api/comments/list?hostname=${hostname}&pathname=${pathname}`);
+    const commentData = yield response.json();
+    if (!Array.isArray(commentData)) {
+        throw new Error('Data is not an array.');
+    }
+    const comments = commentData;
+    console.log({ comments });
+    const item = document.querySelector(`[${TEMPLATE_PREFIX}=${TEMPLATE_SELECTORS.ITEM}]`);
+    const list = document.querySelector(`[${TEMPLATE_PREFIX}=${TEMPLATE_SELECTORS.LIST}]`);
+    if (!item || !list) {
+        !item && console.error('No comment item element found');
+        !list && console.error('No comment list element found');
+        throw new Error('Error getting item and/or list.');
+    }
+    list.innerHTML = '';
+    // item.remove();
+    comments.forEach((comment) => {
+        const clone = item === null || item === void 0 ? void 0 : item.cloneNode(true);
+        const commentText = clone.querySelector(`[${TEMPLATE_PREFIX}=${TEMPLATE_SELECTORS.TEXT}]`);
+        const commentUsername = clone.querySelector(`[${TEMPLATE_PREFIX}=${TEMPLATE_SELECTORS.USERNAME}]`);
+        const commentTimestamp = clone.querySelector(`[${TEMPLATE_PREFIX}=${TEMPLATE_SELECTORS.TIMESTAMP}]`);
+        if (!commentText || !commentUsername || !commentTimestamp) {
+            console.error('Could not find comment elements. Double check your attributes.');
+            throw new Error('Could not find comment elements. Double check your attributes.');
+        }
+        const formattedTime = formatDistanceToNow(comment.timestamp.seconds);
+        commentText.textContent = comment.text;
+        commentUsername.textContent = comment.username;
+        commentTimestamp.textContent = formattedTime;
+        list.appendChild(clone);
+    });
+});
 document.addEventListener('DOMContentLoaded', function () {
     return __awaiter(this, void 0, void 0, function* () {
         console.log(`fetching comments from ${getApiUrl()}`);
-        const hostname = window.location.hostname;
-        const pathname = window.location.pathname;
-        const response = yield fetch(`${getApiUrl()}/api/comments/list?hostname=${hostname}&pathname=${pathname}`);
-        const commentData = yield response.json();
-        if (!Array.isArray(commentData)) {
-            throw new Error('Data is not an array.');
-        }
-        const comments = commentData;
-        console.log({ comments });
-        const item = document.querySelector(`[${TEMPLATE_PREFIX}=${TEMPLATE_SELECTORS.ITEM}]`);
-        const list = document.querySelector(`[${TEMPLATE_PREFIX}=${TEMPLATE_SELECTORS.LIST}]`);
-        if (!item || !list) {
-            !item && console.error('No comment item element found');
-            !list && console.error('No comment list element found');
-            throw new Error('Error getting item and/or list.');
-        }
-        item.remove();
-        comments.forEach((comment) => {
-            const clone = item === null || item === void 0 ? void 0 : item.cloneNode(true);
-            const commentText = clone.querySelector(`[${TEMPLATE_PREFIX}=${TEMPLATE_SELECTORS.TEXT}]`);
-            const commentUsername = clone.querySelector(`[${TEMPLATE_PREFIX}=${TEMPLATE_SELECTORS.USERNAME}]`);
-            const commentTimestamp = clone.querySelector(`[${TEMPLATE_PREFIX}=${TEMPLATE_SELECTORS.TIMESTAMP}]`);
-            if (!commentText || !commentUsername || !commentTimestamp) {
-                console.error('Could not find comment elements. Double check your attributes.');
-                throw new Error('Could not find comment elements. Double check your attributes.');
-            }
-            const formattedTime = formatDistanceToNow(comment.timestamp.seconds);
-            commentText.textContent = comment.text;
-            commentUsername.textContent = comment.username;
-            commentTimestamp.textContent = formattedTime;
-            list.appendChild(clone);
-        });
+        yield fetchComments();
         // FORM
         const form = document.querySelector(`[${FORM_PREFIX}=${FORM_SELECTORS.FORM}]`);
         if (!form) {
@@ -79,9 +83,12 @@ document.addEventListener('DOMContentLoaded', function () {
                 const result = yield response.json();
                 if (result.success) {
                     console.log('Comment added successfully with id: ', result.id);
+                    formUsername.value = '';
+                    formText.value = '';
+                    yield fetchComments();
                 }
                 else {
-                    console.error('Error in adding comment: ', result.message);
+                    console.error('Error adding comment: ', result.message);
                 }
             }
             catch (err) {
